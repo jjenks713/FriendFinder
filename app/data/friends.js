2952ea1d24bdd81b407a1d581ddcc626878bc9b7
Chillen <josh.jenkin@live.com>
2019-02-09 16:42:09 -0700

    everything working, need clean up

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,5 @@
 var friendsArray = [
-     {
+    {
         name: "Josh",
         photo: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3946332.jpg",
         scores: [
@@ -13,8 +13,88 @@ var friendsArray = [
             2,
             3,
             3
-          ]
-        },
+        ]
+    },
+    {
+        name: "Hanna",
+        photo: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3946332.jpg",
+        scores: [
+            4,
+            2,
+            4,
+            5,
+            5,
+            5,
+            4,
+            2,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Jordan",
+        photo: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3946332.jpg",
+        scores: [
+            4,
+            1,
+            5,
+            1,
+            5,
+            3,
+            4,
+            2,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Igor",
+        photo: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3946332.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            1,
+            2,
+            4,
+            3,
+            2,
+            2,
+            5
+        ]
+    },
+    {
+        name: "Lfeon",
+        photo: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3946332.jpg",
+        scores: [
+            1,
+            1,
+            4,
+            1,
+            2,
+            4,
+            2,
+            3,
+            3,
+            4
+        ]
+    },
+    {
+        name: "Rebecca",
+        photo: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX3946332.jpg",
+        scores: [
+            4,
+            2,
+            2,
+            1,
+            4,
+            4,
+            3,
+            2,
+            1,
+            3
+        ]
+    },
 ]
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,16 @@
-var path = require('path');
 
 var friendsData = require("../data/friends");
 
 module.exports = function (app) {
 
 
-    app.get("/survey", function (req, res) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+        // console.log(jsonFriends);
 
-        console.log(friendsData);
+    })
+
+    
         // for (var i = 0; i < friend.length; i ++) {
         //     var currentfriend = friends[1]
         //     app.post(route), function (req,res){
@@ -22,5 +25,37 @@ module.exports = function (app) {
         //         }
         //     }
         // }
-    })
+    app.post("/api/friends", function(req, res) {
+
+        var newFriendScores = req.body.scores;
+        var scoresArray = [];
+        var friendCount = 0;
+        var bestMatch = 0;
+    
+        //runs through all current friends in list
+        for(var i=0; i<friendsData.length; i++){
+          var scoresDiff = 0;
+          //run through scores to compare friends
+          for(var j=0; j<newFriendScores.length; j++){
+            scoresDiff += (Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(newFriendScores[j])));
+          }
+    
+          //push results into scoresArray
+          scoresArray.push(scoresDiff);
+        }
+    
+        //after all friends are compared, find best match
+        for(var i=0; i<scoresArray.length; i++){
+          if(scoresArray[i] <= scoresArray[bestMatch]){
+            bestMatch = i;
+          }
+        }
+    
+        //return bestMatch data
+        var bff = friendsData[bestMatch];
+        res.json(bff);
+    
+        //pushes new submission into the friendsList array
+        friendsData.push(req.body);
+      });
 };
