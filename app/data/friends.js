dd2340219e5b627b430611ce8e9f52670a69bc39
Chillen <josh.jenkin@live.com>
2019-02-09 11:02:15 -0700

    started HTML

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+// var friends = {
+//     name: name-input,
+//     photo: photo-input,
+//     scores: [
+//         1,
+//         2,
+//         3,
+//         4,
+//         5,
+//         6,
+//         7,
+//         8,
+//         9,
+//         10
+// ]
+// };
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+
+
+// for (var i = 0; i < friend.length; i ++) {
+//     var currentfriend = friends[1]
+//     app.post(route), function (req,res){
+
+//         best match
+//         users data
+//         users scores
+//         total difference
+//         for (var i = 0; i < curretfriend.score.length; i++) {
+//         total if friend math.add parseint
+            // push total into empty variable for HTML
+//         }
+//     }
+// }
